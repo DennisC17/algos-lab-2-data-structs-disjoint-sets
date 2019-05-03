fa5e3fa909810b4279240e605daf9f64705a6cc3
DennisC17 <45769800+DennisC17@users.noreply.github.com>
2019-05-03 16:36:24 -0400

    Update DisjointSets.java

--- DisjointSets.java
+++ DisjointSets.java
@@ -7,21 +7,21 @@
  * You have total freedom over how you wanna implement!
  * That means data structures
  */
-import java.util;
+import java.util.*;
 
 public class DisjointSets {
 
   // You want some globals or constructor, determined by which improvements you're implementing
   // using which data structures
-  HashMap<String, DJSnode> DJS = new HasMap<String, DJSnode>();
-  DJSnode newNode;
-  String newString;
+  static HashMap<String, DJSnode> DJS = new HashMap<String, DJSnode>();
+  static DJSnode newNode;
+  static String newString;
 
   /**
    * Make a set given a person's name
    * @param name
    */
-  public void makeSet(String name) {
+  public static void makeSet(String name) {
     if(DJS.containsKey(name)){
       // if set already exists
     }
@@ -36,15 +36,16 @@ public class DisjointSets {
    * @param name person's name
    * @return representative of that person's set
    */
-  public String find(String name) {
+  public static String find(String name) {
     //name exists
     if(DJS.containsKey(name)){
 
       newNode = DJS.get(name);
+
       //if node isn't root
       if(!newNode.isRoot()){
         newString = newNode.getParent();
-        newNode.setParent(find(newString));
+        newNode.setParent( find(newString) );
       }
       //node is root
       else{
@@ -64,18 +65,18 @@ public class DisjointSets {
    * @param name1 first person's name
    * @param name2 second person's name
    */
-  public void union (String name1, String name2) {
+  public static void union(String name1, String name2) {
     String n1 = name1;
     String n2 = name2;
 
     n1 = find(name1);
     n2 = find(name2);
 
-    DJSnode node1 = DJSnode.get(n1);
-    DJSnode node2 = DJSnode.get(n2);
+    DJSnode node1 = DJS.get(n1);
+    DJSnode node2 = DJS.get(n2);
 
-    Int rank1 = node1.getRank();
-    Int rank2 = node2.getRank();
+    int rank1 = node1.getRank();
+    int rank2 = node2.getRank();
 
     if(rank1 >= rank2){
       node2.setParent(n1);
@@ -92,5 +93,26 @@ public class DisjointSets {
 
     // construct the class
     // play around!
+    for(int i=0; i<names.length; i++){
+      makeSet(names[i]);
+    }
+
+    System.out.println("Each person and hir or her respective representative:");
+    for(int i=0; i<names.length; i++){
+      System.out.println(names[i] + "'s representative is " + find(names[i]));
+    }
+
+
+    System.out.println("\nUnion of friend circles: ");
+    for(int i=1; i<names.length; i++){
+      union(names[0], names[i]);
+      System.out.println("Union of " + names[0] + "'s and " + names[i] + "'s friend circles");
+    }
+
+    System.out.println("\nEach person and his or her respective representative:");
+    for(int i=0; i<names.length; i++){
+      System.out.println(names[i] + "'s representative is " + find(names[i]) );
+    }
+
   }
 }
